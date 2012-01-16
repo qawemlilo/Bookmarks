28c1a753406c89f00c8ee6f7a5ae4717e2e11400
Qawelesizwe <qawemlilo@gmail.com>
2012-01-16 11:51:01 +0200

    Refactored add_bookmark.js and tested with JSLint - passed

--- add_bookmark.js
+++ add_bookmark.js
@@ -1,50 +1,61 @@
-(function(x) {
+/*
+****************************************************
+COPYRIGHT © 2011 Raging Flame
+AUTHOR: Qawelesizwe Mlilo
+Email: qawemlilo@gmail.com
+****************************************************
+*/
 
-function saveBookmark(a){
-   var b = false;
-   if (window.XMLHttpRequest) b=new XMLHttpRequest;
+(function (url) {
+    function saveBookmark (urlQuery) {
+        var requestObj = false;
 
-   else if(window.ActiveXObject) b = new ActiveXObject("Microsoft.XMLHTTP");
-   
-   if(b){
-       b.open("GET",a);
-       b.onreadystatechange=function(){};
-       b.send(null)
-   }
-};
+        if (window.XMLHttpRequest) {
+            requestObj = new XMLHttpRequest();
+        }
+        else if (window.ActiveXObject) {
+            requestObj = new ActiveXObject(navigator.userAgent.indexOf("MSIE 5") >= 0 ? "Microsoft.XMLHTTP" : "Msxml2.XMLHTTP");
+        }
+        
+        if (requestObj) {
+            requestObj.open("GET", urlQuery);
+            requestObj.onreadystatechange = function(){};
+            requestObj.send(null);
+        }
+    }
 
-function echoMessage(message) {
-	var msgBox = document.createElement('div');
-	
-	msgBox.style.position = 'absolute';
-	msgBox.style.width = '350px';
-	msgBox.style.height = '20px';
-	msgBox.style.top = '50%';
-	msgBox.style.left = '50%';
-	msgBox.style.margin = '-30px 0px 0px -195px';
-	msgBox.style.backgroundColor = '#f7f7f7';
-	msgBox.style.border = '1px solid #ccc';
-	msgBox.style.color = '#777';
-	msgBox.style.padding = '20px';
-	msgBox.style.fontSize = '16px';
-	msgBox.style.fontFamily = '"Myriad Pro",Arial,Helvetica,sans-serif';
-	msgBox.style.textAlign = 'center';
-	msgBox.style.zIndex = 100000;   
-	msgBox.style.textShadow = '1px 1px 0 white';
-	msgBox.style.borderRadius = "12px";
-	msgBox.style.boxShadow = '0 0 6px #ccc';
-
-	msgBox.setAttribute('onclick','document.body.removeChild(this)');
-	msgBox.appendChild(document.createTextNode(message));
-	document.body.appendChild(msgBox);
-
-	setTimeout(function(){
-		try{
-			document.body.removeChild(msgBox);
-		}	catch(error){}
-	},3000);
-}
-
-saveBookmark( x + '&entry.0.single='+encodeURIComponent(location.href)+'&entry.1.single='+encodeURIComponent(document.title) );
-echoMessage("Bookmark Saved");
-})(googleFormActionUrl_367342iurw34);
+    function echoMessage (message) {
+        var msgBox = document.createElement('div'), msgBoxStyle = msgBox.style; 
+        
+        msgBoxStyle.position = 'absolute';
+        msgBoxStyle.height = '20px';
+        msgBoxStyle.top = '50%';
+        msgBoxStyle.left = '50%';
+        msgBoxStyle.margin = '-30px 0px 0px -195px';
+        msgBoxStyle.backgroundColor = '#f7f7f7';
+        msgBoxStyle.border = '1px solid #ccc';
+        msgBoxStyle.color = '#777';
+        msgBoxStyle.padding = '20px';
+        msgBoxStyle.fontSize = '16px';
+        msgBoxStyle.fontFamily = '"Myriad Pro",Arial,Helvetica,sans-serif';
+        msgBoxStyle.textAlign = 'center';
+        msgBoxStyle.zIndex = 100000;
+        msgBoxStyle.textShadow = '1px 1px 0 white';
+        msgBoxStyle.borderRadius = "12px";
+        msgBoxStyle.boxShadow = '0 0 6px #ccc';
+        
+        msgBox.setAttribute('onclick','document.body.removeChild(this)');
+        msgBox.appendChild(document.createTextNode(message));
+        document.body.appendChild(msgBox);
+        
+        setTimeout(function () {
+            try {
+			    document.body.removeChild(msgBox);
+            } catch (error) {}
+	    }, 3000);
+    }
+    
+    saveBookmark(url + '&entry.0.single='+encodeURIComponent(location.href) + '&entry.1.single=' + encodeURIComponent(document.title));
+    echoMessage("Bookmark Saved");
+    
+}(googleFormActionUrl_367342iurw34));
